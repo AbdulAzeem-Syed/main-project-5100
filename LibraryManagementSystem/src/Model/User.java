23bfe53e51e5e4bb886fbeaebe967cc463969082
AbdulAzeem-Syed <syed.abd@northeastern.edu>
2023-11-29 18:48:19 -0500

    Simple project structure setup

---
+++ LibraryManagementSystem/src/Model/User.java
@@ -0,0 +1,35 @@
+package Model;
+
+/**
+ *
+ * @author Abdul
+ */
+public class User {
+    private int userId;
+    private String name;
+    private String role;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
