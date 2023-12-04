b69d300db336301d43fe1a8db49ae899c04691b6
Wajid Hussain <52674797+wajidengg@users.noreply.github.com>
2023-12-04 03:39:15 -0500

    Updated admin reports panel with edit and delete buttons

--- LibraryManagementSystem/src/UI/adminReportsPanel.java
+++ LibraryManagementSystem/src/UI/adminReportsPanel.java
@@ -4,7 +4,14 @@
  */
 package UI;
 
+import Model.UserMessage;
+import Util.UserMessageDBConnector;
+import java.awt.desktop.UserSessionEvent;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
 
 /**
  *
@@ -15,8 +22,12 @@ public class adminReportsPanel extends javax.swing.JPanel {
     /**
      * Creates new form adminReportsPanel
      */
+    private ArrayList<UserMessage> usermessage;
+    private UserMessage selectedUserMessage;
+    
     public adminReportsPanel(JPanel bottomPanel) {
         initComponents();
+        populateTable();
     }
 
     /**
@@ -30,35 +41,51 @@ public class adminReportsPanel extends javax.swing.JPanel {
 
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        reportsTable = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        commentTypeTextField = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        emailTextField = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextPane1 = new javax.swing.JTextPane();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        commentTextBox = new javax.swing.JTextPane();
+        viewReportButton = new javax.swing.JButton();
+        deleteReportButton = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(204, 204, 255));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setText("Reports");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        reportsTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "queryId", "email", "message", "type"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Object.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(reportsTable);
+        if (reportsTable.getColumnModel().getColumnCount() > 0) {
+            reportsTable.getColumnModel().getColumn(0).setResizable(false);
+            reportsTable.getColumnModel().getColumn(1).setResizable(false);
+        }
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel2.setText("View Report");
@@ -69,11 +96,21 @@ public class adminReportsPanel extends javax.swing.JPanel {
 
         jLabel5.setText("Comment:");
 
-        jScrollPane2.setViewportView(jTextPane1);
+        jScrollPane2.setViewportView(commentTextBox);
 
-        jButton1.setText("View");
+        viewReportButton.setText("View");
+        viewReportButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                viewReportButtonActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("Delete");
+        deleteReportButton.setText("Delete");
+        deleteReportButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteReportButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -89,8 +126,8 @@ public class adminReportsPanel extends javax.swing.JPanel {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(viewReportButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(deleteReportButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                     .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -98,8 +135,8 @@ public class adminReportsPanel extends javax.swing.JPanel {
                                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField1)
-                                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
+                                    .addComponent(commentTypeTextField)
+                                    .addComponent(emailTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
                                     .addComponent(jScrollPane2)))
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 668, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(17, Short.MAX_VALUE))
@@ -122,27 +159,62 @@ public class adminReportsPanel extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(commentTypeTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(emailTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel5)
                             .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
+                        .addComponent(deleteReportButton)
                         .addGap(20, 20, 20)
-                        .addComponent(jButton1)))
+                        .addComponent(viewReportButton)))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void deleteReportButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteReportButtonActionPerformed
+        // TODO add your handling code here:
+        int selectedIndex = reportsTable.getSelectedRow();
+        if (selectedIndex == -1) {
+            JOptionPane.showMessageDialog(this, "Please select a user to delete", "Cannot delete user", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        try {
+            selectedUserMessage = usermessage.get(selectedIndex);
+            UserMessageDBConnector.deleteUser(selectedUserMessage);
+            JOptionPane.showMessageDialog(null, "User deleted successfully", "Successfully deleted", JOptionPane.INFORMATION_MESSAGE);
+            clearFields();
+            populateTable();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage());
+        }
+        
+    }//GEN-LAST:event_deleteReportButtonActionPerformed
+
+    private void viewReportButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewReportButtonActionPerformed
+        // TODO add your handling code here:
+        int selectedIndex = reportsTable.getSelectedRow();
+        if (selectedIndex == -1) {
+            JOptionPane.showMessageDialog(this, "Please select a user to edit", "Cannot edit user", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        selectedUserMessage = usermessage.get(selectedIndex);
+        commentTypeTextField.setText(selectedUserMessage.getType());
+        emailTextField.setText(selectedUserMessage.getEmail());
+        commentTextBox.setText(selectedUserMessage.getMessage());
+        
+    }//GEN-LAST:event_viewReportButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTextPane commentTextBox;
+    private javax.swing.JTextField commentTypeTextField;
+    private javax.swing.JButton deleteReportButton;
+    private javax.swing.JTextField emailTextField;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -150,9 +222,33 @@ public class adminReportsPanel extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JTable reportsTable;
+    private javax.swing.JButton viewReportButton;
     // End of variables declaration//GEN-END:variables
+
+    private void populateTable() {
+        try {
+            this.usermessage = UserMessageDBConnector.getAllusers();
+            DefaultTableModel model = (DefaultTableModel) reportsTable.getModel();
+            model.setRowCount(0);
+            for (UserMessage u: usermessage) {
+                Object[] row = new Object[4];
+                row[0] = u.getQueryId();
+                row[1] = u.getEmail();
+                row[2] = u.getMessage();
+                row[3] = u.getType();
+                model.addRow(row);
+            }
+            clearFields();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage());
+        }
+    }
+
+    private void clearFields() {
+        commentTypeTextField.setText("");
+        emailTextField.setText("");
+        commentTextBox.setText("");
+        selectedUserMessage = null;
+    }
 }
